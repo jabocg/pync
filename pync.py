ae56014cdf12b0d1739b13f446e4d720b83db879
jabocg <jabocg@gmail.com>
2015-09-07 02:29:49 -0700

    Added --quiet argument, overrides --verbose

--- pync.py
+++ pync.py
@@ -7,52 +7,56 @@ parser = argparse.ArgumentParser()
 parser.add_argument("source", help="directory to sync from")
 parser.add_argument("dest", help="directory to sync to")
 parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+parser.add_argument("-q", "--quiet", help="suppress output, except errors, OVERRIDES --verbose", action="store_true")
 args = parser.parse_args()
 
 
-def sync(src,dest,verbose=False):
+def sync(src,dest,verbose=False,quiet=False):
     for f in os.listdir(src):   # every file and directory in the path 'src'
         pathname = os.path.join(src,f)  # combine 'src' and 'f' to create full path for current item
         pathname2 = os.path.join(dest,f)    # combine 'dest' and 'f' to create full path for current item
-        if verbose:
+        if verbose and not quiet:
             print("Source: ",pathname)
         if os.path.isdir(pathname): # if the current item is a directory
-            if verbose:
+            if verbose and not quiet:
                 print(pathname," is a directory")
             if not os.path.exists(pathname2):   # if the destination directory does not exist
-                print("creating ",pathname2)
-                if verbose:
+                if not quiet:
+                    print("creating ",pathname2)
+                if verbose and not quiet:
                     print("recursing into ",pathname)
                 os.makedirs(pathname2)  # create destination directory
-            sync(pathname,pathname2,verbose)    # recursively sync data in directory
+            sync(pathname,pathname2,verbose,quiet)    # recursively sync data in directory
         elif os.path.isfile(pathname):  # if the current item is a file
-            if verbose:
+            if verbose and not quiet:
                 print(pathname, " is a file")
-            copyto(pathname,pathname2,verbose)  # copy to destination
+            copyto(pathname,pathname2,verbose,quiet)  # copy to destination
         else:
             print('Error: skipping ' , pathname)   # else print error
     
 
-def copyto(src,dest,verbose=False):
+def copyto(src,dest,verbose=False,quiet=False):
     if not os.path.exists(dest):
-        if verbose:
+        if verbose and not quiet:
             print(dest, " does not exist, creating")
         shutil.copy2(src,dest)
-        print(src, " -> ", dest)
+        if not quiet:
+            print(src, " -> ", dest)
     else:
-        if verbose:
+        if verbose and not quiet:
             print(dest, " exists")
         if not filecmp.cmp(src,dest):
-            if verbose:
+            if verbose and not quiet:
                 print(src, " and ", dest, " are not identical")
             if os.path.getctime(src) > os.path.getctime(dest):
-                if verbose:
+                if verbose and not quiet:
                     print(src, " is newer than ", dest)
                 shutil.copy2(src,dest)
-                print(src, " -> ", dest)
-        elif verbose:
+                if not quiet:
+                    print(src, " -> ", dest)
+        elif verbose and not quiet:
             print(src, " and ", dest, " are identical")
     
 
 # run the sync with the args
-sync(args.source,args.dest,args.verbose)
+sync(args.source,args.dest,args.verbose,args.quiet)
